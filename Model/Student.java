ec6ac923bf6035d0314877df38fe7138d755e8cc
Diogo Lemos <145502790+dblemos@users.noreply.github.com>
2023-10-06 12:55:31 +0100

    Add files via upload

---
+++ Model/Student.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Student extends Class1 {
+
+    /**
+     * Default constructor
+     */
+    public Student() {
+    }
+
+    /**
+     * 
+     */
+    public int matNo;
+
+}
